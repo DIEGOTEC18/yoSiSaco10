117775d17fe9d6f911fead96dc0aa9bb2842f19e
DIEGOVELAZ <42215166+DIEGOTEC18@users.noreply.github.com>
2019-08-18 19:53:34 -0500

    Update constants.js

--- Landing/constants.js
+++ Landing/constants.js
@@ -1,6 +1,6 @@
 //Constantes de cognito:
 
-var CognitoUserPoolId = "us-east-1_ZQhF1ytAU";
-var CognitoIdentityPoolId = "us-east-1:1fb72f3b-3b07-45e6-ac60-ec79cd1c4d9c";
-var CognitoUserPoolAppClientId = "3qbjeosqbjhq4k2em225v0ks7d";
-var awsRegion = "us-east-1";
+var CognitoUserPoolId = "";
+var CognitoIdentityPoolId = "";
+var CognitoUserPoolAppClientId = "";
+var awsRegion = "";
